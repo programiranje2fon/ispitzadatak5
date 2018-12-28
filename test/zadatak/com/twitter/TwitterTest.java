56a0575c96e54b5aabae4b18ca7da5a3697e0e3e
Dragoljub Djuric <djuric2103@gmail.com>
2018-12-28 15:36:59 +0100

    Prva verzija

--- test/zadatak/com/twitter/TwitterTest.java
+++ test/zadatak/com/twitter/TwitterTest.java
@@ -15,7 +15,6 @@ import java.util.GregorianCalendar;
 import java.util.LinkedList;
 import java.util.List;
 
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -44,7 +43,7 @@ public class TwitterTest {
 	public void tearDown() throws Exception {
 		instance = null;
 		System.setOut(originalOut);
-	    System.setErr(originalErr);
+		System.setErr(originalErr);
 	}
 
 	@Test
@@ -76,22 +75,22 @@ public class TwitterTest {
 		}
 		return true;
 	}
-	
+
 	public void inicijalizuj() {
 		instance.unesi("Marko", "#p2 prva");
 		instance.unesi("Zdravko", "#p2 druga");
 		instance.unesi("Marko", "#p3 treca");
 	}
-	
+
 	@Test(timeout = 2000)
 	public void metoda_unesi() {
 		inicijalizuj();
 		List<TwitterPoruka> stvarna = (List<TwitterPoruka>) TestUtil.getFieldValue(instance, "poruke");
 		List<TwitterPoruka> kopija = new LinkedList<>();
-		for(int i = 0; i < stvarna.size(); i++) {
+		for (int i = 0; i < stvarna.size(); i++) {
 			kopija.add(stvarna.get(i));
 		}
-		
+
 		String korisnik = "Marko";
 		String poruka = "zdravo";
 		GregorianCalendar vreme = new GregorianCalendar();
@@ -101,78 +100,77 @@ public class TwitterTest {
 		twitterPoruka.setVreme(vreme);
 		instance.unesi(korisnik, poruka);
 		kopija.add(twitterPoruka);
-		
+
 		assertTrue("Nakon izvrsetka metode unesi sadrzaj liste poruka nije ispravan", iste(stvarna, kopija));
 		String ispis = outContent.toString().trim();
-		
-		for(TwitterPoruka t : stvarna) {
-			if(ispis.indexOf(t.toString()) == -1){
+
+		for (TwitterPoruka t : stvarna) {
+			if (ispis.indexOf(t.toString()) == -1) {
 				assertTrue("Metoda unesi ne ispisuje sve poruke na ekranu", false);
 			}
 		}
 	}
-	
+
 	private LinkedList<TwitterPoruka> pomocnaTag(int maxBroj, String tag) {
 		LinkedList<TwitterPoruka> poruke = (LinkedList<TwitterPoruka>) TestUtil.getFieldValue(instance, "poruke");
 		LinkedList<TwitterPoruka> rezultat = new LinkedList<>();
-		try{
+		try {
 			PrintWriter out = new PrintWriter(new FileWriter("pretraga.txt"));
-			
+
 			tag = '#' + tag + ' ';
 			int brojac = 0;
-			
-			for (int i = 0; i < poruke.size(); i++) 
-				if (poruke.get(i).getPoruka().indexOf(tag)!=-1)
-					if (brojac < maxBroj){
+
+			for (int i = 0; i < poruke.size(); i++)
+				if (poruke.get(i).getPoruka().indexOf(tag) != -1)
+					if (brojac < maxBroj) {
 						rezultat.add(poruke.get(i));
 						brojac++;
-					}
-					else break;
-			
+					} else
+						break;
+
 			out.close();
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return rezultat;
 	}
-	
-	
+
 	private void vratiPorukeTag(int maxBroj) {
 		String tag = "p2";
 		inicijalizuj();
 		LinkedList<TwitterPoruka> rezultat = pomocnaTag(maxBroj, tag);
 		instance.vratiPoruke(maxBroj, tag);
-		
+
 		String fajl = new String();
 		int brojLinija = 0;
-		try(BufferedReader br = new BufferedReader(new FileReader("pretraga.txt"))) {
-		    for(String line; (line = br.readLine()) != null; ) {
-		       fajl = fajl.concat(line);
-		       if(!line.isEmpty())
-		    	   brojLinija++;
-		    }
-		}catch(IOException ex) {
+		try (BufferedReader br = new BufferedReader(new FileReader("pretraga.txt"))) {
+			for (String line; (line = br.readLine()) != null;) {
+				fajl = fajl.concat(line);
+				if (!line.isEmpty())
+					brojLinija++;
+			}
+		} catch (IOException ex) {
 			ex.printStackTrace();
 			assertTrue("Nije pronadjen fajl pretraga.txt", false);
 		}
-		
+
 		assertTrue("Fajl sadrzi vise poruka nego sto je potrebno", brojLinija == rezultat.size());
-		
-		for(TwitterPoruka t : rezultat) {
+
+		for (TwitterPoruka t : rezultat) {
 			assertTrue("Fajl ne sadrzi sve potrebne poruke", fajl.indexOf(t.toString()) != -1);
 		}
 	}
-	
+
 	@Test
 	public void metoda_vratiPorukeTag_maxBrojManjiOdBrojaPoruka() {
 		vratiPorukeTag(1);
 	}
-	
+
 	@Test
 	public void metoda_vratiPorukeTag_maxBrojJednakBrojaPoruka() {
 		vratiPorukeTag(2);
 	}
-	
+
 	@Test
 	public void metoda_vratiPorukeTag_maxBrojVeciOdBrojaPoruka() {
 		vratiPorukeTag(3);
@@ -184,43 +182,44 @@ public class TwitterTest {
 		GregorianCalendar trenutniDatum = new GregorianCalendar();
 		int godina = trenutniDatum.get(GregorianCalendar.YEAR);
 		int mesec = trenutniDatum.get(GregorianCalendar.MONTH);
-		
-		if (mesec!=0) mesec--;
-		else{
+
+		if (mesec != 0)
+			mesec--;
+		else {
 			mesec = 11;
 			godina--;
 		}
-		
+
 		for (int i = 0; i < poruke.size(); i++)
-			if (poruke.get(i).getKorisnik().equals(korisnik) &&
-					poruke.get(i).getVreme().get(GregorianCalendar.YEAR)==godina &&
-					poruke.get(i).getVreme().get(GregorianCalendar.MONTH)==mesec){
-					TwitterPoruka nova = poruke.get(i);
-					
-					for (int j = 0; j < novePoruke.size(); j++)
-						if (nova.getVreme().before(novePoruke.get(i).getVreme())){
-							novePoruke.add(i, nova);
-							break;
-						}
-						
-					if (!novePoruke.contains(nova))
-						novePoruke.addLast(nova);
+			if (poruke.get(i).getKorisnik().equals(korisnik)
+					&& poruke.get(i).getVreme().get(GregorianCalendar.YEAR) == godina
+					&& poruke.get(i).getVreme().get(GregorianCalendar.MONTH) == mesec) {
+				TwitterPoruka nova = poruke.get(i);
+
+				for (int j = 0; j < novePoruke.size(); j++)
+					if (nova.getVreme().before(novePoruke.get(i).getVreme())) {
+						novePoruke.add(i, nova);
+						break;
+					}
+
+				if (!novePoruke.contains(nova))
+					novePoruke.addLast(nova);
 			}
-		
+
 		return novePoruke;
 	}
-	
+
 	@Test(timeout = 2000)
 	public void metoda_vratiPorukeKorisnik() {
 		inicijalizuj();
-		List<TwitterPoruka> ocekivana= pomocna("Marko");
+		List<TwitterPoruka> ocekivana = pomocna("Marko");
 		List<TwitterPoruka> stvarna = instance.vratiPoruke("Marko");
-		
-		for(TwitterPoruka t : ocekivana) {
+
+		for (TwitterPoruka t : ocekivana) {
 			assertTrue("Vracena lista ne sadrzi sve potrebne poruke", stvarna.contains(ocekivana));
 		}
-		
-		for(TwitterPoruka t : stvarna) {
+
+		for (TwitterPoruka t : stvarna) {
 			assertTrue("Vracena lista sadrzi poruke koje ne bi trebala", ocekivana.contains(stvarna));
 		}
 	}
